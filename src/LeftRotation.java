dfca9768e1888630230ef3ace53a54c8c44d9ab5
Gabriel Aguirre <gabrielaguirre921@gmail.com>
2017-11-27 20:55:50 -0500

    Added description for LeftRotation

--- src/LeftRotation.java
+++ src/LeftRotation.java
@@ -1,27 +1,13 @@
 import java.util.Scanner;
 
+/**
+ * A left rotation operation on an array of size shifts each of the array's elements unit to the left.
+ * For example, if left rotations are performed on array , then the array would become.
+ *
+ * Given an array of integers and a number, , perform left rotations on the array.
+ * Then print the updated array as a single line of space-separated integers.
+ */
 public class LeftRotation {
-    public static int[] arrayLeftRotation(int[] a, int n, int k) {
-        if (n != k) {
-            a = shift(a, n, k);
-        }
-        return a;
-    }
-
-    public static int[] shift(int[] a, int n, int k) {
-        int[] b = new int[n];
-        int x = 0;
-        for (int i = k; i < n; i++) {
-            b[x] = a[i];
-            x++;
-        }
-
-        for (int j = 0; j < k; j++) {
-            b[x] = a[j];
-            x++;
-        }
-        return b;
-    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -39,5 +25,27 @@ public class LeftRotation {
 
         System.out.println();
 
+    }
+
+    public static int[] arrayLeftRotation(int[] array, int n, int k) {
+        if (n != k) {
+            array = shift(array, n, k);
+        }
+        return array;
+    }
+
+    public static int[] shift(int[] a, int n, int k) {
+        int[] b = new int[n];
+        int x = 0;
+        for (int i = k; i < n; i++) {
+            b[x] = a[i];
+            x++;
+        }
+
+        for (int j = 0; j < k; j++) {
+            b[x] = a[j];
+            x++;
+        }
+        return b;
     }
 }
